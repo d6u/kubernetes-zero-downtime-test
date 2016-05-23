e7496bd7f0fc52665c30fa7bd37c4681355c43e4
Daiwei <daiweilu123@gmail.com>
2016-05-23 10:02:51 -0400

    Initial commit

---
+++ hello-node/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+setTimeout(() => {
+  const http = require('http');
+
+  const hostname = '0.0.0.0';
+  const port = 3000;
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World v8\n');
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}, 15000);
